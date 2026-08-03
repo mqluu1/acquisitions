689f06ca836e4a9291700fb3bcc32a7b6e0466e3
mqluu1 <boskies11@gmail.com>
2026-08-02 18:27:52 -0700

    Implement ESLint and Prttier

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello from Acquistions!');
-})
+  res.status(200).send('Hello from Acquisitions!');
+});
 
 export default app;
